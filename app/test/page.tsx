325b2cc7f73621e896d1a022d1eef0c83dd73990
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-08-15 16:55:15 +1000

    Fixed build error

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,12 +4,12 @@ import { Calculator } from "@/components/Calculator3/Calculator";
 import { Calculator as Calc } from "@/components/Calculator/Calculator";
 import { Calculator as CalcOpt } from "@/components/CalculatorPaymentOptions/Calculator";
 import { UnsavedChangesProvider } from '@/components/unsavedChanges';
-import Login from '@/components/LoginComponent';
+// import Login from '@/components/LoginComponent';
 
 export default function Page() {
     return(
         <UnsavedChangesProvider>
-            <Login businessName="DBM" />
+            {/* <Login businessName="DBM" /> */}
             <Calculator prefilled={false}/>
             <Calc/>
             <CalcOpt />
